a440db492a3029ca44cef59948347ed88b6f69b8
umangaamatya <amatyaumanga@gmail.com>
2024-10-07 14:49:01 +0545

    Update columns

--- app/models/user.py
+++ app/models/user.py
@@ -6,6 +6,6 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(100), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
+    first_name = Column(String(50))
+    last_name = Column(String(100))
+    email = Column(String(100), unique=True)
